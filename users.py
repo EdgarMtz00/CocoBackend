bcf5c29fe6aad4d095edf6c50b86aa78e9ea0d5f
Alan Ramírez Herrera <alan5142@hotmail.com>
2019-03-29 23:06:02 -0600

    CRU finished

--- users.py
+++ users.py
@@ -1,6 +1,9 @@
 from pyramid.response import Response
 from pyramid.request import Request
+from sqlalchemy import text
 from sqlalchemy.engine import ResultProxy
+from sqlalchemy.sql.elements import TextClause
+
 from database import db
 import json
 
@@ -13,24 +16,78 @@ def _get_user(request: Request) -> Response:
         return Response(status=404)
     else:
         try:
-            get_user: ResultProxy = db.execute('SELECT * from cocollector."Estado"')
+            stmt: TextClause = text('SELECT "ID", '
+                                    '"Nombre_usuario",'
+                                    '"Correo",'
+                                    '"Nombre", '
+                                    '"Apellido_paterno", '
+                                    '"Apellido_materno",'
+                                    '"Tipo" from cocollector."Usuario" where "ID" = :id')
+            stmt = stmt.bindparams(id=user_id)
+
+            get_user: ResultProxy = db.execute(stmt)
             result = get_user.fetchall()
-            return Response(status=200, body=to_json(result), content_type='text/json')
-        except Exception:
-            return Response(status=404)
+            return Response(status=200, body=to_json(result[0]), content_type='text/json')
+        except Exception as e:
+            print(e)
+            return Response(status=404, content_type='text/plain')
 
 
 def _create_user(request: Request) -> Response:
-    user_data = json.loads(request.json_body)
     try:
-        insert_user = db.execute('')
+        user_data = request.json_body
+        stmt: TextClause = text('INSERT into cocollector."Usuario"("Nombre_usuario",'
+                                '"Correo",'
+                                '"Contrasena",'
+                                '"Nombre",'
+                                '"Apellido_paterno",'
+                                '"Apellido_materno",'
+                                '"Tarjeta_credito",'
+                                '"Fecha_Expiracion",'
+                                '"Tipo") VALUES (:nombre_usuario, :correo, :contrasena, '
+                                ':nombre, :apellido_paterno, :apellido_materno, :tarjeta, :fecha_expiracion, "Usuario")')
+
+        stmt = stmt.bindparams(nombre_usuario=user_data['nombreUsuario'], correo=user_data['correo'],
+                               nombre=user_data['nombre'], apellido_paterno=user_data['apellidoPaterno'],
+                               apellido_materno=['apellidoMaterno'], tarjeta=['tarjeta'],
+                               fecha_expiracion=user_data['fecha_expiracion'])
+        db.execute(stmt)
+        return Response(status=200)
     except Exception:
         return Response(status=400)
+
+
+def _modify_user(request: Request) -> Response:
+    try:
+        user_data = request.json_body
+        user_stmt = text('SELECT * from cocollector."Usuario" where "ID" = :id').bindparams(id=user_data['id'])
+        user: dict = json.loads(to_json(db.execute(user_stmt)))
+        if 'nombre' in user_data:
+            user['Nombre'] = user_data['nombre']
+        if 'apellidoPaterno' in user_data:
+            user['Apellido_paterno'] = user_data['apellidoPaterno']
+        if 'nombreDeUsuario' in user_data:
+            user['Nombre_usuario'] = user_data['nombreDeUsuario']
+        if 'correo' in user_data:
+            user['Correo'] = user_data['correo']
+        if 'contrasena' in user_data:
+            user['Contrasena'] = user_data['contrasena']
+
+        update_stmt = text(
+            'UPDATE cocollector."Usuario" SET "Nombre" = :nombre, "Apellido_paterno" = :apellido_paterno, "Nombre_usuario" = :nombre_usuario, "Correo" = :correo, "Contrasena" = :contrasena where "ID" = :id'
+        ).bindparams(nombre=user['Nombre'], apellido_paterno=user['Apellido_paterno'], nombre_usuario=user['Nombre_usuario'], correo=user['Correo'], contrasena=user['Contrasena'])
+        db.execute(update_stmt)
+
+    except Exception as e:
+        print(e)
+        return Response(status=404, content_type='text/json')
 
 
 def user_entry(request: Request):
     if request.method == 'GET':
         return _get_user(request)
     elif request.method == 'POST':
-        _get_user(request)
-    return Response()
+        return _create_user(request)
+    elif request.method == 'PUT':
+        return _modify_user(request)
+    return Response(status=405, content_type='text/json')
