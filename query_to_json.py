35dafb7e4754aeded498261127aae21c3e83c243
Alan Ramírez Herrera <alan5142@hotmail.com>
2019-03-29 22:07:07 -0600

    To json

---
+++ query_to_json.py
@@ -0,0 +1,12 @@
+import json
+from sqlalchemy.engine import ResultProxy
+
+
+def to_json(query: ResultProxy):
+    dic = []
+
+    for row in query:
+        print(row.keys()[0])
+        for index, key in enumerate(row):
+            dic.append({row.keys()[index]: key})
+    return json.dumps(dic)
