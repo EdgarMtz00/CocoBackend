d6712039f97b9d4c70dc50bd6e3ef12b23508143
Alan Ramírez Herrera <alan5142@hotmail.com>
2019-03-29 20:39:06 -0600

    Yey :D

--- users.py
+++ users.py
@@ -1,26 +1,23 @@
 from pyramid.response import Response
 from pyramid.request import Request
+from sqlalchemy.engine import ResultProxy
 from database import db
 import json
 
 
-def _get_user(request: Request):
+def _get_user(request: Request) -> Response:
     user_id = request.params.get('id', -1)
     if user_id == -1:
-        try:
-            get_all_users = db.execute('SELECT "Nombre", "Nombre_usuario", "Correo" from cocollector."Usuario"')
-            return Response(status=200, body= json.dump(get_all_users), content_type='text/json')
-        except Exception:
-            return Response(status=404)
+        return Response(status=404)
     else:
         try:
-            get_user = db.execute('SELECT * from cocollector."Usuario" where id = ' + user_id)
-            return Response(status=200, body= json.dump(get_user), content_type='text/json')
+            get_user = db.execute('SELECT * from cocollector."Usuario" where "ID"= ' + user_id)
+            return Response(status=200, body=json.dumps(get_user), content_type='text/json')
         except Exception:
             return Response(status=404)
 
 
 def user_entry(request: Request):
     if request.method == 'GET':
-        return user_entry()
+        return _get_user(request)
     return Response()
