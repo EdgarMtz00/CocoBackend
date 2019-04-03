1deb1763307c3a844501d0c35e8064a11975fd99
TheRaven0 <kevingenial01@gmail.com>
2019-04-02 23:02:39 -0600

    CRUD terminado *dab*.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 from users import user_entry
-
+from products import product_entry
 
 def hello_world(request):
     return Response(
@@ -14,10 +14,13 @@ def hello_world(request):
 
 if __name__ == '__main__':
     with Configurator() as config:
-        config.add_route('hello', '/producto')  # localhost:6543/
-        config.add_view(hello_world, route_name='hello')
+        #config.add_route('hello', '/hello')  # localhost:6543/
+        #config.add_view(hello_world, route_name='hello')
+        config.add_route('products', '/productos')  # localhost:6543/
+        config.add_view(product_entry, route_name='products')
         config.add_route('users', '/usuarios')
         config.add_view(user_entry, route_name='users')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
+
---
+++ products.py
@@ -0,0 +1,98 @@
+from pyramid.response import Response
+from pyramid.request import Request
+from sqlalchemy import text
+from sqlalchemy.engine import ResultProxy
+from sqlalchemy.sql.elements import TextClause
+
+from database import db
+import json
+
+from query_to_json import to_json
+
+def _get_product(request: Request) -> Response:
+    product_id = request.params.get('id', -1)
+    if product_id == -1:
+        return Response(status=404)
+    else:
+        try:
+            stmt: TextClause = text('SELECT * from cocollector."Producto" where "ID_Producto" = :id')
+            stmt = stmt.bindparams(id=product_id)
+            print (":)")
+            get_product: ResultProxy = db.execute(stmt)
+            result = get_product.fetchall()
+            print(result)
+            return Response(status=200, body=to_json(result), content_type='text/json')
+        except Exception as e:
+            print(e)
+            return Response(status=404, content_type='text/plain')
+
+def _create_product(request: Request) -> Response:
+    try:
+        product_data = request.json_body
+        stmt: TextClause = text('INSERT INTO cocollector."Producto"("Descripcion",'
+                                '"Nombre",'
+                                '"Precio",'
+                                '"Stock",'
+                                '"Categoria") VALUES (:descripcion, :nombre, :precio, :stock, :categoria)')
+
+        stmt = stmt.bindparams(descripcion = product_data['descripcion'], 
+                               nombre = product_data['nombre'],
+                               precio = product_data['precio'], 
+                               stock = product_data['stock'],
+                               categoria = product_data['categoria'])
+
+        print(stmt)
+        print(product_data)
+        db.execute(stmt)
+
+        return Response(status=200)
+    except Exception:
+        return Response(status=400)
+
+def _modify_product(request: Request) -> Response:
+    try:
+        product_data = request.json_body
+        
+        stmt = text('UPDATE cocollector."Producto" SET'
+                           '"Descripcion" = :descripcion,'
+                           '"Nombre" = :nombre,'
+                           '"Precio" = :precio,'
+                           '"Stock" = :stock,'
+                           '"Categoria" = :categoria WHERE "ID_Producto" = :id')
+
+        stmt = stmt.bindparams(descripcion = product_data['descripcion'],
+                                             nombre = product_data['nombre'], 
+                                             precio = product_data['precio'], 
+                                             stock = product_data['stock'], 
+                                             categoria = product_data['categoria'], 
+                                             id = product_data['id'])
+        db.execute(stmt)
+
+        return Response(status=200)
+    except Exception as e:
+        print(e)
+        return Response(status=404, content_type='text/json')
+
+def _delete_product(request: Request) -> Response:
+    try:
+        product_data = request.json_body
+        stmt = text('DELETE FROM cocollector."Producto" WHERE "ID_Producto" = :id')
+        stmt = stmt.bindparams(id = product_data['id'])
+        print(":3")
+        db.execute(stmt)
+
+        return Response(status=200)
+    except Exception as e:
+        print(e)
+        return Response(status=404, content_type='text/json')
+
+def product_entry(request: Request):
+    if request.method == 'GET':
+        return _get_product(request)
+    elif request.method == 'POST':
+        return _create_product(request)
+    elif request.method == 'PUT':
+        return _modify_product(request)
+    elif request.method == 'DELETE':
+        return _delete_product(request)
+    return Response(status=405, content_type='text/json')
