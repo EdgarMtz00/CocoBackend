565d7ec312eb88ee88b05caa35ccb994b8387050
eldesdoll <38787939+eldesdoll@users.noreply.github.com>
2019-04-06 19:25:03 -0600

    pedido

--- venv/Scripts/easy_install-3.7-script.py
+++ venv/Scripts/easy_install-3.7-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\alan5\Documents\CETI\8vo\Distribuidos\2do_Parcial\CocoBackend\venv\Scripts\python.exe
+#!C:\Users\eldes\Documents\GitHub\CocoBackend\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install-3.7'
 __requires__ = 'setuptools==39.1.0'
 import re
